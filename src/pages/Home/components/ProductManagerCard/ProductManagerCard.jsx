139bce72deecfc12fe684f9c223e63c15a1a5e0d
abhishek pandey <kakupandey2017@gmail.com>
2026-01-04 12:29:08 +0530

    verions 1.1.1

--- src/pages/Home/components/ProductManagerCard/ProductManagerCard.jsx
+++ src/pages/Home/components/ProductManagerCard/ProductManagerCard.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom';
-import { LayoutGrid } from 'lucide-react';
+import { FilePenLine,PackagePlus   } from 'lucide-react';
 import './ProductManagerCard.css';
 
 const ProductManagerCard = () => {
@@ -12,14 +12,14 @@ const ProductManagerCard = () => {
       role="button"
     >
       <div className="pm-card-content">
-        <h3 className="pm-card-title">Product Manager</h3>
-        <p className="pm-card-desc">Add product, edit and Delete Products</p>
+        <h3 className="pm-card-title">Product Manager <span> <FilePenLine color='#2563eb'/> </span> </h3>
+        <p className="pm-card-desc">Add product, Edit and Delete Products</p>
         <div
           className="pm-card-action "
           style={{ display: 'flex', alignItems: 'center', marginTop: '40px' }}
         >
           {' '}
-          <LayoutGrid /> <span>Go to Dashboard →</span>
+          <PackagePlus /> <span>Go to Dashboard →</span>
         </div>
       </div>
     </div>
